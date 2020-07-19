74e3db2067272881b535081ee7b83a9aaf7e0a62
CAGMagtibay <41831788+CAGMagtibay@users.noreply.github.com>
2020-07-19 18:45:13 -0400

    Create hangman.py

---
+++ stage-2/hangman.py
@@ -0,0 +1,10 @@
+# Write your code here
+print("H A N G M A N")
+
+word = "python"
+guess = input("Guess the word: ")
+
+if guess == word:
+    print("You survived!")
+else:
+    print("You are hanged!")
